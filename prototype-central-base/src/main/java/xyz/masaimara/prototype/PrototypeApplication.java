c533051b7c877728c95b7fc239b70206f1c91240
yuk <themostss@hotmail.com>
2018-08-23 00:22:28 +0800

    1.add redis support

--- prototype-central-base/src/main/java/xyz/masaimara/prototype/PrototypeApplication.java
+++ prototype-central-base/src/main/java/xyz/masaimara/prototype/PrototypeApplication.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.ValueOperations;
 import xyz.masaimara.prototype.data.mapper.TestMapper;
 
 @SpringBootApplication
@@ -13,10 +15,17 @@ public class PrototypeApplication implements CommandLineRunner {
     private final Logger logger = LoggerFactory.getLogger(PrototypeApplication.class);
 
     private final TestMapper testMapper;
+//    @Autowired
+//    private RedisTest test;
+
+    private final ListOperations<String, String> listOperations;
+    private ValueOperations<String, String> valueOperations;
 
     @Autowired
-    public PrototypeApplication(TestMapper testMapper) {
+    public PrototypeApplication(TestMapper testMapper, ListOperations<String, String> listOperations, ValueOperations<String, String> valueOperations) {
         this.testMapper = testMapper;
+        this.listOperations = listOperations;
+        this.valueOperations = valueOperations;
     }
 
     public static void main(String[] args) {
@@ -26,5 +35,9 @@ public class PrototypeApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         logger.info("result:{}", testMapper.findJobById("0e9573bb7ad5409daf3299d1a0e4a48f"));
+
+        valueOperations.set("0e9573bb7ad5409daf3299d1a0e4a48f", "http://masaimara.xyz");
+        listOperations.leftPush("spring.boot.redis.test","0e9573bb7ad5409daf3299d1a0e4a48f");
+        String key = "spring.boot.redis.test";
     }
 }
--- prototype-central-base/src/main/java/xyz/masaimara/prototype/application/config/data/redis/RedisConfiguration.java
+++ prototype-central-base/src/main/java/xyz/masaimara/prototype/application/config/data/redis/RedisConfiguration.java
@@ -1,13 +1,36 @@
 package xyz.masaimara.prototype.application.config.data.redis;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @Configuration
+@PropertySource("classpath:/redis.properties")
+//@ConfigurationProperties("spring.redis")
 public class RedisConfiguration {
 
 //    @Bean
-//    @Qualifier("stringRedisTemplate")
-//    public RedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        return new StringRedisTemplate(redisConnectionFactory);
+//    public LettuceConnectionFactory redisConnectionFactory() {
+//        return new LettuceConnectionFactory(new RedisStandaloneConfiguration());
 //    }
+
+    @Bean
+    @Qualifier("listOperations")
+    public ListOperations listOperations(StringRedisTemplate template){
+        return template.opsForList();
+    }
+
+    @Bean
+    @Qualifier("valueOperations")
+    public ValueOperations<String, String> valueOperations(StringRedisTemplate template) {
+        return template.opsForValue();
+    }
 }
